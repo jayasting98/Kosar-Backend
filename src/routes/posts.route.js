1c4be0a1b05dbe2134a43619d757bc9ebed5cfe8
Jay Ting <65202977+jayasting98@users.noreply.github.com>
2022-06-19 17:20:02 +0800

    Add route for creating a post

--- src/routes/posts.route.js
+++ src/routes/posts.route.js
@@ -1,5 +1,9 @@
+const postsController = require("../controllers/posts.controller");
+
 const { Router } = require("express");
 
 const router = Router();
+
+router.post("/", postsController.createPost);
 
 module.exports = router;
