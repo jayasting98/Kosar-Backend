994a534a8ec9ce8d86de5703ef45ced6c8d6df52
Jay Ting <65202977+jayasting98@users.noreply.github.com>
2022-06-19 15:21:24 +0800

    Create posts route

--- src/routes/v1.route.js
+++ src/routes/v1.route.js
@@ -1,3 +1,5 @@
+const postsRouter = require("./posts.route");
+
 const express = require("express");
 
 const v1 = express();
@@ -5,5 +7,7 @@ const v1 = express();
 v1.get("/", (req, res) => {
   res.send("Hello World!");
 });
+
+v1.use("/posts", postsRouter);
 
 module.exports = v1;
