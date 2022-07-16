5d4c84ba8af2792e79edfaf5bdbc23424c295f52
Jay Ting <65202977+jayasting98@users.noreply.github.com>
2022-07-17 00:42:27 +0800

    Add user creation controller method

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -1,1 +1,26 @@
 const usersService = require('../services/users.service');
+
+exports.createUser = async (req, res) => {
+  const emailAddress = req.body.emailAddress;
+  const username = req.body.username;
+  const plaintextPassword = req.body.password;
+  let user;
+  try {
+    user = await usersService.createUser(
+        emailAddress,
+        username,
+        plaintextPassword,
+    );
+  } catch (error) {
+    res.status(500)
+        .end();
+  }
+  const response = {
+    userId: user.userId,
+    emailAddress: user.emailAddress,
+    username: user.username,
+    dateTimeCreated: user.dateTimeCreated,
+  };
+  res.status(201)
+      .json(response);
+};
