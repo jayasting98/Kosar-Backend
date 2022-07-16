39ca018494e21337ca4a6b2e1e00ae4bbe71d4db
Jay Ting <65202977+jayasting98@users.noreply.github.com>
2022-07-16 20:17:02 +0800

    Add users table

--- src/db/00.00.00.sql
+++ src/db/00.00.00.sql
@@ -1,3 +1,12 @@
+CREATE TABLE IF NOT EXISTS users (
+    userial BIGSERIAL PRIMARY KEY,
+    uid UUID UNIQUE NOT NULL DEFAULT uuid_generate_v4(),
+    email_address TEXT NOT NULL,
+    username TEXT NOT NULL,
+    password_hash TEXT NOT NULL,
+    date_time_created TIMESTAMP(6) WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
 CREATE TABLE IF NOT EXISTS posts (
     pserial BIGSERIAL PRIMARY KEY,
     pid UUID UNIQUE NOT NULL DEFAULT uuid_generate_v4(),
