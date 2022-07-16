83a849fed36b34a3fdc69febdef8515f2f801a31
Jay Ting <65202977+jayasting98@users.noreply.github.com>
2022-07-17 00:41:56 +0800

    Add user creation in users service

--- src/services/users.service.js
+++ src/services/users.service.js
@@ -1,2 +1,40 @@
 const auth = require('./auth.service');
 const db = require('./db.service');
+const idUtil = require('../utils/id.util');
+const passwordUtil = require('../utils/password.util');
+
+const CREATE_USER_SQL_TEMPLATE =
+`INSERT INTO users (
+  email_address,
+  username,
+  password_hash
+) VALUES (
+  $1,
+  $2,
+  $3
+) RETURNING
+  *;`;
+
+const convertUserRowToUser = (userRow) => {
+  const userId = idUtil.convertFromRawId(userRow.uid);
+  const user = {
+    userId: userId,
+    emailAddress: userRow.email_address,
+    username: userRow.username,
+    passwordHash: userRow.password_hash,
+    dateTimeCreated: userRow.date_time_created,
+  };
+  return user;
+};
+
+exports.createUser = async (emailAddress, username, plaintextPassword) => {
+  const passwordSalt = await passwordUtil.generateSalt();
+  const passwordHash =
+      await passwordUtil.generateHash(plaintextPassword, passwordSalt);
+  const dbQueryParameters = [emailAddress, username, passwordHash];
+  const dbResult = await db.query(CREATE_USER_SQL_TEMPLATE, dbQueryParameters);
+  const userRow = dbResult.rows[0];
+  const user = convertUserRowToUser(userRow);
+  await auth.createUser(user.userId);
+  return user;
+};
