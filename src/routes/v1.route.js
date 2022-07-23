16cc8821917f93132faef87ee20d4e76a9116cb0
Jay Ting <65202977+jayasting98@users.noreply.github.com>
2022-07-23 21:10:30 +0800

    Add route for creating sessions

--- src/routes/v1.route.js
+++ src/routes/v1.route.js
@@ -1,4 +1,5 @@
 const postsRouter = require('./posts.route');
+const sessionsRouter = require('./sessions.route');
 const usersRouter = require('./users.route');
 
 const express = require('express');
@@ -12,6 +13,7 @@ v1.get('/', (req, res) => {
 });
 
 v1.use('/posts', postsRouter);
+v1.use('/sessions', sessionsRouter);
 v1.use('/users', usersRouter);
 
 module.exports = v1;
