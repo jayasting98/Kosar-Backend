8d63e6f6ad27dc32a839a57989b87915703d0f03
Jay Ting <65202977+jayasting98@users.noreply.github.com>
2022-07-25 18:08:07 +0800

    Integrate authorization-checking middleware into routes

--- src/routes/v1.route.js
+++ src/routes/v1.route.js
@@ -1,3 +1,5 @@
+// eslint-disable-next-line max-len
+const authorizationMiddleware = require('../middlewares/authorization.middleware');
 const postsRouter = require('./posts.route');
 const usersRouter = require('./users.route');
 const v1UnauthorizedRouter = require('./v1.unauthorized-route');
@@ -9,6 +11,8 @@ const v1 = express();
 v1.use(express.json());
 
 v1.use(v1UnauthorizedRouter);
+
+v1.use(authorizationMiddleware);
 
 v1.use('/posts', postsRouter);
 v1.use('/users', usersRouter);
