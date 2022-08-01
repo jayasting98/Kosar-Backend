367f5bfb46b0498ee0e4ecefdd7da8a3ed86d41d
Jay Ting <65202977+jayasting98@users.noreply.github.com>
2022-08-01 18:11:00 +0800

    Make user email addresses and usernames unique

--- src/db/00.00.00.sql
+++ src/db/00.00.00.sql
@@ -1,8 +1,8 @@
 CREATE TABLE IF NOT EXISTS users (
     userial BIGSERIAL PRIMARY KEY,
     uid UUID UNIQUE NOT NULL DEFAULT uuid_generate_v4(),
-    email_address TEXT NOT NULL,
-    username TEXT NOT NULL,
+    email_address TEXT UNIQUE NOT NULL,
+    username TEXT UNIQUE NOT NULL,
     password_hash TEXT NOT NULL,
     date_time_created TIMESTAMP(6) WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
