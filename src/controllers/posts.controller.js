6717ee10b49b56357afaaf373309aab305c8de58
Jay Ting <65202977+jayasting98@users.noreply.github.com>
2022-08-02 14:57:20 +0800

    Change post creation to account for authors

--- src/controllers/posts.controller.js
+++ src/controllers/posts.controller.js
@@ -4,33 +4,40 @@ const idUtil = require('../utils/id.util');
 
 const CREATE_POST_SQL_TEMPLATE =
 `INSERT INTO posts (
-  message
+  message,
+  author_uid
 ) VALUES (
-  $1
+  $1,
+  $2
 ) RETURNING *;`;
 
 const GET_POSTS_SQL =
 `SELECT
   p.pid,
   p.message,
+  p.author_uid,
   p.date_time_created
 FROM
   posts p;`;
 
 const convertPostRowToPost = (postRow) => {
   const postId = idUtil.convertFromRawId(postRow.pid);
-  return {
+  const authorUid = idUtil.convertFromRawId(postRow.author_uid);
+  const post = {
     postId: postId,
     message: postRow.message,
+    authorUid: authorUid,
     dateTimeCreated: postRow.date_time_created,
   };
+  return post;
 };
 
 exports.createPost = async (req, res) => {
   const message = req.body.message;
+  const authorUid = idUtil.convertToRawId(req.user.uid);
   let result;
   try {
-    result = await db.query(CREATE_POST_SQL_TEMPLATE, [message]);
+    result = await db.query(CREATE_POST_SQL_TEMPLATE, [message, authorUid]);
   } catch (err) {
     res.status(500)
         .end();
